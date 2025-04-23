6e49741192c400dc00709fa00ebfd94e073177cd
Nida <tazen01@pfw.edu>
2025-04-23 09:12:00 -0400

    added to allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-a%fp7xfqd1myw7u5lgl_s3n-n+le-n881aruxwvccz6h3!ofc!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mastodonhub-nizj.onrender.com]
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 if DEBUG:
